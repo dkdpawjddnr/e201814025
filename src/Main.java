4259e34d5d25fa024b2fd3e510cc96f545d07666
Ji, jeonguk <dkdpawjddnr@naver.com>
2024-09-25 09:46:40 +0900

    "fifth"

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("지정욱");
         World.run();
+        World.run2();
     }
 }
---
+++ src/World.java
@@ -0,0 +1,10 @@
+public class World {
+
+    public static void run() {
+        System.out.println("201814025");
+    }
+
+    public static void run2(){
+        System.out.println("hello world");
+    }
+}
